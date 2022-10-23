0e309ff041b44375d058bf94dbc2deaab4d2e8f7
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-23 14:40:50 +0600

    create data hotels.json and show display ui and with deploy server

--- index.js
+++ index.js
@@ -6,9 +6,25 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+// first data load and show server port 5000
+const hotels = require('./data/hotels.json');
+
 app.get('/', (req, res)=>{
     res.send('Travel guru server is running')
 });
+
+// first data load and show server port 5000
+app.get('/hotels', (req, res)=>{
+    res.send(hotels);
+})
+
+// find for every signle hotel
+app.get('/hotels/:id', (req, res)=>{
+    const id =req.params.id;
+    const hotel =hotels.find(ht=> ht.id == id);
+    // console.log(hotel);
+    res.send(hotel);
+})
 
 app.listen(port, ()=>{
     console.log(`Travel guru running on port, ${port}`);
