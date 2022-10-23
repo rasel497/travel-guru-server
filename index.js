1301181b8feed3a5055e0ed63b2f178ebd3368a5
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-23 12:41:41 +0600

    initial setup commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app =  express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/', (req, res)=>{
+    res.send('Travel guru server is running')
+});
+
+app.listen(port, ()=>{
+    console.log(`Travel guru running on port, ${port}`);
+})
+
+
